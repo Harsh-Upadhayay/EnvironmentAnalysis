d13076a5c919d7794395539bcfb712ccaba2157d
Harsh-Upadhayay <harshupadhayay906@gmail.com>
2021-10-11 19:34:52 +0530

    Moved csv file to datasets folder

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ def get_Temperature_Graph():
 
     Dict = {'January':'01','February':'02','March':'03','April':'04','May':'05','June':'06',
             'July':'07','August':'08','September':'09','October':'10','November':'11','December':'12'}
-    with open('GlobalLandTemperaturesByCountry.csv') as File:
+    with open('datasets/GlobalLandTemperaturesByCountry.csv') as File:
         File_Ptr = csv.reader(File)
         Count , Cmonth = 0,0
         Year , Avg_Temperature = [],[]
