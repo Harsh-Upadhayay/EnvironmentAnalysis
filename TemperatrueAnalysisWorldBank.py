624b99bf19d43ad92dfe57d550203df4f7d1e2e9
Harsh Upadhayay <harshupadhayay906@gmail.com>
2021-10-20 21:38:01 +0530

    Added analysis of WorldBankData, & renamed WordlBankdata folder
    Added support to analyse single country as well as multiple countries

--- TemperatrueAnalysisWorldBank.py
+++ TemperatrueAnalysisWorldBank.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 countriesMap = {}
-dataPath = 'datasets/WhoDataBank/tas_timeseries_annual_cru_1901-2020_'
+dataPath = 'datasets/WorldBankData/tas_timeseries_annual_cru_1901-2020_'
 months = {'January':'-01-', 'February':'-02-', 'March'    :'-03-',
           'April'  :'-04-', 'May'     :'-05-', 'June'     :'-06-',
           'July'   :'-07-', 'August'  :'-08-', 'September':'-09-',
@@ -15,7 +15,7 @@ def __setCountriesMap():
     Call only once
     """
     global CountriesMap
-    with open("datasets\WhoDataBank\countriesCode.txt", "r") as file:
+    with open("datasets\WorldBankData\countriesCode.txt", "r") as file:
         data = file.readlines()
         for line in data:
                 word = line.split('^^^^^')
@@ -25,47 +25,51 @@ def __setCountriesMap():
         countriesMap[y] = x
 
 
-def TemperaturesWhoCountries(country, dateStart = '1700', dateEnd = '2100'):
+def TemperatureByCountryWBD(country, dateStart = 1700, dateEnd = 2100):
 
-    global citiesMap
+    global countriesMap
     if not countriesMap:
         __setCountriesMap()
 
     try:
-        df = pd.read_csv(dataPath + countriesMap[country] + '.csv')
+        country = countriesMap[country]
+        df = pd.read_csv(dataPath + country + '.csv', header=1)
     except:
         return 0
-    df.columns = df.iloc[0]
-    df = df.iloc[1:]
-    df['Date'] = df.index
-    df = df.reset_index(drop=True)
+    
+    df.rename(columns={'Unnamed: 0':'Date'},inplace=True)
+
+    if 'Administrative unit not available' in df.columns:
+        df.drop(['Administrative unit not available'], axis=1, inplace=True)
+    
+    df = df[df['Date'].between(dateStart, dateEnd)]    
+    df.reset_index(drop=True , inplace=True)
     return df
 
 
-# def TemperaturesByUScities(cityTuple, month, day = 0, dateStart = '1700-01-01', dateEnd = '2010-01-01'):
+def TemperatureByCountriesWBD(countryTuple, dateStart = 1700, dateEnd = 2100):
     
-#     if isinstance(cityTuple, str):
-#         return TemperaturesByUScity(cityTuple, month, day, dateStart, dateEnd)
+    if isinstance(countryTuple, str):
+        return TemperatureByCountryWBD(countryTuple, dateStart, dateEnd)
     
-#     retdf = pd.DataFrame
-#     firstIter = True
+    retdf = pd.DataFrame
+    firstIter = True
 
  
-#     for city in cityTuple:
-#         cur_df = TemperaturesByUScity(city, month, day, dateStart, dateEnd)
-#         cur_df[city] = cur_df[['tmin', 'tmax']].mean(axis=1)
-#         cur_df.drop(cur_df.columns[[1,2,3]], axis=1, inplace=True)
-           
-#         if firstIter:
-#             retdf = cur_df
-#             firstIter = False
-#         else:
-#             retdf = retdf.merge(cur_df, how='outer')
+    for country in countryTuple:
+        cur_df = TemperatureByCountryWBD(country, dateStart, dateEnd)
+        cur_df.drop(cur_df.iloc[:, 2:], inplace = True, axis = 1)
+
+        if firstIter:
+            retdf = cur_df
+            firstIter = False
+        else:
+            retdf = retdf.merge(cur_df)
     
-#     return retdf
+    return retdf
 
 
-df = TemperaturesWhoCountries('Aruba')
-df.plot( x = 'Date')
+df = TemperatureByCountriesWBD(['India', 'United States'])
+df.plot(x = 'Date', subplots=True)
 plt.show()
 print(df)
