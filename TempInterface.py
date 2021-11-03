f31e9fa099a11f800b1d6ee89d4ee375e9f0e8e3
Harsh Upadhayay <harshupadhayay906@gmail.com>
2021-11-03 13:11:02 +0530

    WDB Countries canalysis added

--- TempInterface.py
+++ TempInterface.py
@@ -28,48 +28,41 @@ def _msg():
     msg.showerror("Error", "Enter correct name!")
 
 
-def WBD_TA_Country(country_name, rainAnalysis):
+def WBD_TA_Country(countries, placeHolder):
     
-    country = country_name.get()
-    df = globalWBD.TemperatureByCountriesWBD([country])
+    countries = countries.get().split(',')
+    idx = 0
+    for country in countries:
+        countries[idx] = country.strip()
+        idx += 1
+
+    df = globalWBD.TemperatureByCountriesWBD(countries)
     figure = plt.Figure(figsize=(10, 6), dpi=100)
     ax = figure.add_subplot(111)
-    chart_type = FigureCanvasTkAgg(figure, rainAnalysis)
+    chart_type = FigureCanvasTkAgg(figure, placeHolder)
     chart_type.get_tk_widget().grid(column=0, row=1, columnspan=4)
 
-    df.plot(x = 'Date', subplots=True, title=country,  ylabel='Temperature in degree celsius', ax=ax)
+    df.plot(x = 'Date', subplots=True,  ylabel='Temperature in degree celsius', ax=ax)
     
 
-def countryBox():
-    countryName = StringVar()
+def countryBox(placeHolder, firstMessage, secondMessage, firstPos = 0):
+    try :    
+        countryName = StringVar()
 
-    f1 = LabelFrame(Tmp_analysis, text="Analysis by country")
-    f1.grid(column=0, row=0, padx=8, pady=2)
+        f1 = LabelFrame(placeHolder, text=firstMessage)
+        f1.grid(column=firstPos, row=0, padx=8, pady=2)
 
-    l1 = Label(f1, text="Enter Country Name :").grid(
-        column=0, row=0, padx=8, pady=4)
+        l1 = Label(f1, text=secondMessage).grid(
+            column=0, row=0, padx=8, pady=4)
 
-    e1 = Entry(f1, textvariable=countryName).grid(
-        column=1, row=0, padx=8, pady=4)
+        e1 = Entry(f1, textvariable=countryName).grid(
+            column=1, row=0, padx=8, pady=4)
 
-    b1 = Button(f1, text="show", command=lambda : WBD_TA_Country(countryName, Tmp_analysis)).grid(
-        row=1, columnspan=2, pady=2)
+        b1 = Button(f1, text="show", command=lambda : WBD_TA_Country(countryName, placeHolder)).grid(
+            row=1, columnspan=2, pady=2)
+    except :
+        _msg()
 
-
-def countryComparisonBox():
-    countriesName = StringVar()
-
-    f1 = LabelFrame(Tmp_analysis, text="Analysis by country")
-    f1.grid(column=0, row=0, padx=8, pady=2)
-
-    l1 = Label(f1, text="Enter Country Name :").grid(
-        column=0, row=0, padx=8, pady=4)
-
-    e1 = Entry(f1, textvariable=countriesName).grid(
-        column=1, row=0, padx=8, pady=4)
-
-    b1 = Button(f1, text="show", command=lambda : WBD_TA_Country(countryName, Tmp_analysis)).grid(
-        row=1, columnspan=2, pady=2)
-
-countryBox()
+countryBox(Tmp_analysis, "Country Analysis", "Enter Contry Name", 0)
+countryBox(Tmp_analysis, "Countries Comparision", "Enter Contries Name (Comma Seperated)", 1)
 win.mainloop()
