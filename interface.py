0d1fc23c070665f55256d625abf2ca0c122d0bd7
Harsh Upadhayay <harshupadhayay906@gmail.com>
2021-11-05 21:54:05 +0530

    modifiedAbout

--- interface.py
+++ interface.py
@@ -21,14 +21,22 @@ def About(notebook):
     frame2 = tkinter.LabelFrame(AbtFrame, text="Tech stack")
     frame2.grid(column=0, row=1, padx=8, pady=4)
 
-    para2_1 = tkinter.Label(frame2, text="Numpy").grid(
+    para2_1 = tkinter.Label(frame2, text="Numpy & Scipy : Data Analysis").grid(
         column=0, row=0, sticky=tkinter.W, padx=10, pady=10)
-    para2_2 = tkinter.Label(frame2, text="Pandas").grid(
+    para2_2 = tkinter.Label(frame2, text="Pandas : Data Manipulation").grid(
         column=0, row=1, sticky=tkinter.W, padx=10, pady=10)
-    para2_3 = tkinter.Label(frame2, text="Matplotlib").grid(
+    para2_3 = tkinter.Label(frame2, text="Matplotlib : Data Representation").grid(
         column=0, row=2, sticky=tkinter.W, padx=10, pady=10)
-    para2_4 = tkinter.Label(frame2, text="Python GUI (Tkinter)").grid(
+    para2_4 = tkinter.Label(frame2, text="CSV : File Handling").grid(
         column=0, row=2, sticky=tkinter.W, padx=10, pady=10)
+    para2_4 = tkinter.Label(frame2, text="Pyautogui : GUI Automation").grid(
+        column=0, row=3, sticky=tkinter.W, padx=10, pady=10)
+    para2_4 = tkinter.Label(frame2, text="Selenium : Browser Automation").grid(
+        column=0, row=4, sticky=tkinter.W, padx=10, pady=10)
+    para2_4 = tkinter.Label(frame2, text="Tkinter : Python GUI").grid(
+        column=0, row=5, sticky=tkinter.W, padx=10, pady=10)
+    para2_4 = tkinter.Label(frame2, text="Exception Handling").grid(
+        column=0, row=6, sticky=tkinter.W, padx=10, pady=10)
 
     frame3 = tkinter.LabelFrame(AbtFrame, text="Participants or Group info")
     frame3.grid(column=0, row=2, padx=8, pady=4)
@@ -104,7 +112,7 @@ def main():
         
     win = tkinter.Tk()
     win.title("Python Mini Project")
-    win.attributes("-zoomed",True)
+    # win.attributes("-zoomed",True)
     style = ttk.Style()
     style.theme_settings
     ("default",
